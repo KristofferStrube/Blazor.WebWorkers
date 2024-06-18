aee6acbd51538f40642ec08ecf2d57908abbe494
KristofferStrube <kristoffer.strube@gmail.com>
2024-06-19 00:19:28 +0200

    Title changes.

--- src/KristofferStrube.Blazor.WebWorkers/Extensions/IJSRuntimeExtensions.cs
+++ src/KristofferStrube.Blazor.WebWorkers/Extensions/IJSRuntimeExtensions.cs
@@ -7,12 +7,12 @@ internal static class IJSRuntimeExtensions
     internal static async Task<IJSObjectReference> GetHelperAsync(this IJSRuntime jSRuntime)
     {
         return await jSRuntime.InvokeAsync<IJSObjectReference>(
-            "import", "./_content/KristofferStrube.Blazor.Worker/KristofferStrube.Blazor.WebWorkers.js");
+            "import", "./_content/KristofferStrube.Blazor.WebWorkers/KristofferStrube.Blazor.WebWorkers.js");
     }
 
     internal static async Task<IJSInProcessObjectReference> GetInProcessHelperAsync(this IJSRuntime jSRuntime)
     {
         return await jSRuntime.InvokeAsync<IJSInProcessObjectReference>(
-            "import", "./_content/KristofferStrube.Blazor.Worker/KristofferStrube.Blazor.WebWorkers.js");
+            "import", "./_content/KristofferStrube.Blazor.WebWorkers/KristofferStrube.Blazor.WebWorkers.js");
     }
 }
