dda860f8e9aadbf17ebe42c59b285179ce194d7f
KristofferStrube <KristofferStrube@users.noreply.github.com>
2024-10-02 20:56:05 +0000

    deploy: fe2fd397351a29af57f70182c7e9b93315312201

---
+++ _content/KristofferStrube.Blazor.WebWorkers/KristofferStrube.Blazor.WebWorkers.JobWorker.js
@@ -0,0 +1,27 @@
+﻿const params = new Proxy(new URLSearchParams(self.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+let assembly = params.assembly;
+
+import { dotnet } from "../../_framework/dotnet.js"
+
+let instance = await dotnet.create()
+
+let objects = [];
+
+instance.setModuleImports("boot.js", {
+    createObject: () => {
+        let newObject = {};
+        objects.push(newObject);
+        return newObject;
+    },
+    disposeObject: (obj) => {
+        objects = objects.filter(item => item != obj);
+    },
+    postMessage: (message) => self.postMessage(message),
+    registerOnMessage: (handler) => self.addEventListener("message", handler)
+});
+
+self.postMessage("ready");
+
+await instance.runMainAndExit(`${assembly}.wasm`, []);
